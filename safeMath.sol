edcb85851649ec2069a1287ff69c8cddddc56ed1
Alber Erre <arblugo@gmail.com>
2018-02-20 12:17:26 +0100

    math

---
+++ safeMath.sol
@@ -0,0 +1,33 @@
+contract SafeMath {
+
+    uint constant DAY_IN_SECONDS = 86400;
+
+    function mul(uint256 a, uint256 b) constant internal returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) constant internal returns (uint256) {
+        assert(b != 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) constant internal returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) constant internal returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+
+    function mulByFraction(uint256 number, uint256 numerator, uint256 denominator) internal returns (uint256) {
+        return div(mul(number, numerator), denominator);
+    }
+
+}
